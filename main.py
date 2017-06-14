01072f8fc32587dfb5b96bc650a3845f4c703010
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-06-14 21:08:18 +0100

    Last commit? Until confirm with Mr Bufford.

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ primes = get_primes(max_number_to_find_primes_to)           # populates the arra
 
 count = primes.__len__()                                    # declares a variable "count" and makes it equal to the number of items in the "primes" array
 
-public_key = primes [randint(0,count - 1)]           # randomly picks a prime from the array "primes" and puts it in "public_key"
+public_key = primes [randint(0,count - 1)]                  # randomly picks a prime from the array "primes" and puts it in "public_key"
 print("public key: " + str(public_key) + " (always a prime number)")
-base_number = randint (0, count * 10)                # picks a random number between 0 and 10 * the number of primes in array "primes" and puts it in "base_number"
+base_number = randint (0, count * 10)                       # picks a random number between 0 and 10 * the number of primes in array "primes" and puts it in "base_number"
 print("base number: " + str(base_number) + " (base number, can be anything)")
-alice_key = primes [randint(0,count - 1)]            # randomly picks a prime from the array "primes" and puts it in "alice_key"
+alice_key = primes [randint(0,count - 1)]                   # randomly picks a prime from the array "primes" and puts it in "alice_key"
 print("Alice's key: " + str(alice_key) + " (always a prime number)")
-bob_key = primes [randint(0,count - 1)]              # randomly picks a prime from the array "primes" and puts it in "bob_key"
+bob_key = primes [randint(0,count - 1)]             # randomly picks a prime from the array "primes" and puts it in "bob_key"
 print("Bob's key: " + str(bob_key) + " (always a prime number)")
 alice_send = (base_number**alice_key)%public_key            # calculates the number Alice will send and puts it in "alice_send"
 print ("Alice sends to Bob the base number to the power of  Alice's key modulo the public key: " + str(alice_send))
--- prime.py
+++ prime.py
@@ -4,10 +4,12 @@ __author__ = "Mr Bancroft"
 # max_number_to_find_primes_to variable in main.py
 
 def get_primes(n):
-    numbers = set(range(n,1,-1))                                # "set()" declares an unordered collection with no duplicates. "range()" creates a range of numbers starting at the passed in value n and ending at the item at index 1, counting back 1 every step so if 10 were passed it would create 2,3,4,5,6,7,8,9,10
-    primes = []                                                 # declares an array "primes"
-    while numbers:                                              # while loop that works through the range "numbers"
-        p = numbers.pop()                                       # creates a variable "p" and places the last value in "numbers" into is using "pop()"
-        primes.append(p)                                        # adds (appends) "p" to the "primes" array
-        numbers.difference_update(set(range(p * 2, n + 1, p)))  # uses "difference_update" to
-    return primes
+    number_range = set(range(n,1,-1))                                   # "set()" declares an unordered collection with no duplicates. "range()" creates a range of numbers
+                                                                        # starting at the passed in value n and ending at the item at index 1, counting back 1 every step so if 10
+                                                                        # were passed it would create 2,3,4,5,6,7,8,9,10, this unordered collection of numbers is put into "number_range"
+    primes = []                                                         # declares an array "primes"
+    while number_range:                                                 # while loop that works through the range "number_range"
+        p = number_range.pop()                                          # creates a variable "p" and places the first value in "numbers" into is using "pop()"
+        primes.append(p)                                                # adds (appends) "p" to the "primes" array
+        number_range.difference_update(set(range(p * 2, n + 1, p)))     # uses "difference_update" to remove the non prime numbers from the array "number_range"
+    return primes                                                       # returns the array of "primes"
