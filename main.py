f37cd8d61506d36e2513866e03a8c9a4cd0f14a4
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-06-14 13:54:04 +0100

    Added check for success and more comments for further clarity

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ primes = prime.get_primes(max_number_to_find_primes_to)     # populates the arra
 
 count = primes.__len__()                                    # declares a variable "count" and makes it equal to the number of items in the "primes" array
 
-publickey = primes [random.randint(0,count - 1)]
-print("public key: " + str(publickey) + " (always a prime number)")
-basenumber = random.randint (0, count * 10)
-print("base number: " + str(basenumber) + " (base number, can be anything)")
-aliceKey = primes [random.randint(0,count - 1)]
-print("Alice's key: " + str(aliceKey) + " (always a prime number)")
-bobKey = primes [random.randint(0,count - 1)]
-print("Bob's key: " + str(bobKey) + " (always a prime number)")
-aliceSend1=(basenumber**aliceKey)%publickey
-print ("Alice sends to Bob the base number to the power of  Alice's key modulo the public key: " + str(aliceSend1))
-bobSend1=(basenumber**bobKey)%publickey
-print ("Bob sends to Alice the base number to the power of Bob's key modulo the public key: " + str(bobSend1))
-alicedecode = (bobSend1**aliceKey)%publickey
-print("Alice's code: " + str(alicedecode) + " (the result of what Bob sent to the power of Alice's key modulo the public key)")
-bobdecode = (aliceSend1**bobKey)%publickey
-print ("Bob's code: " + str(bobdecode) + " (the result of what Alice sent to the power of Bob's key modulo the public key)")
+public_key = primes [random.randint(0,count - 1)]           # randomly picks a prime from the array "primes" and puts it in "public_key"
+print("public key: " + str(public_key) + " (always a prime number)")
+base_number = random.randint (0, count * 10)                # picks a random number between 0 and 10 * the number of primes in array "primes" and puts it in "base_number"
+print("base number: " + str(base_number) + " (base number, can be anything)")
+alice_key = primes [random.randint(0,count - 1)]            # randomly picks a prime from the array "primes" and puts it in "alice_key"
+print("Alice's key: " + str(alice_key) + " (always a prime number)")
+bob_key = primes [random.randint(0,count - 1)]              # randomly picks a prime from the array "primes" and puts it in "bob_key"
+print("Bob's key: " + str(bob_key) + " (always a prime number)")
+alice_send = (base_number**alice_key)%public_key            # calculates the number Alice will send and puts it in "alice_send"
+print ("Alice sends to Bob the base number to the power of  Alice's key modulo the public key: " + str(alice_send))
+bob_send = (base_number**bob_key)%public_key                # calculates the number Bob will send and puts it in "bob_send"
+print ("Bob sends to Alice the base number to the power of Bob's key modulo the public key: " + str(bob_send))
+alice_decode = (bob_send**alice_key)%public_key             # calculates the number Alice will end up with after checking the information passed from Bob and puts it in "alice_decode"
+print("Alice's code: " + str(alice_decode) + " (the result of what Bob sent to the power of Alice's key modulo the public key)")
+bob_decode = (alice_send**bob_key)%public_key               # calculates the number Bob will end up with after checking the information passed from Alice and puts it in "bob_decode"
+print ("Bob's code: " + str(bob_decode) + " (the result of what Alice sent to the power of Bob's key modulo the public key)")
+has_passed = alice_decode == bob_decode                     # calculate a boolean (true of false) which reflects whether Bob and Alice's decoded numbers are the same
+print ("The key exchanged worked correctly: " + str(has_passed))
